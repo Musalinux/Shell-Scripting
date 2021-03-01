923c07a9472a4363cfa3a206f5a1bb4968ba1571
Musalinux <55594624+Musalinux@users.noreply.github.com>
2021-03-01 14:25:27 +0530

    Create Rocket-launch-script.sh
    Created my first shell script

---
+++ Rocket-launch-script.sh
@@ -0,0 +1,13 @@
+vi create-and-launch-rocket
+
+mkdir lunar-mission
+rocket-add lunar-mission
+rocket-start-power lunar-mission
+rocket-internal-power lunar-mission
+rocket-start-sequence lunar-mission
+rocket-start-engine lunar-mission
+rocket-lift-off lunar-mission
+rocket-status lunar-mission
+
+chmod +x create-and-launch-rocket //This gives execution rights to the the script file
+./create-and-launch-rocket
